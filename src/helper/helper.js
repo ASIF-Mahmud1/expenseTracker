bdf5cffeeecd49e5e73188b0c45c1c9de9d05552
Asif <1421015@iub.edu.bd>
2022-03-05 15:43:01 +0600

    [done] helper function to categorise expense based on Day and Week

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -24,6 +24,100 @@ const getListOfCategory = (items) => {
     return finalResult
 }
 
+const getDayDate=(date)=>{
+  let day= new Date(date)
+  return  day.getFullYear()+'-'+(day.getMonth()+1)+'-'+day.getDate();
+}
+
+const getWeekDate = (date) => {
+
+    let currentdate = new Date(date)
+    let oneJan = new Date(currentdate.getFullYear(), 0, 1);
+    let numberOfDays = Math.floor((currentdate - oneJan) / (24 * 60 * 60 * 1000));
+    let result = Math.ceil((currentdate.getDay() + 1 + numberOfDays) / 7);
+   
+    return result
+
+}
+
+
+const getExpenseByDay=(items)=>{
+    const finalResult= []
+    let uniqueDate={}
+    items.forEach((ele)=>{
+        var dayDate =getDayDate(ele.date)
+        uniqueDate[dayDate]=[]
+    })
+
+    items.forEach((ele)=>{
+        let correspindingDate= getDayDate (ele.date)
+        uniqueDate[correspindingDate].push(ele)
+    })
+
+    for (let x in uniqueDate) {
+        let sum = 0
+        uniqueDate[x].forEach((item) => {
+            sum = sum + Number(item.price)
+        })
+        const expense = {
+            title: "Date: "+ x,
+            totalPrice: sum,
+            other: uniqueDate[x]
+        }
+        finalResult.push(expense)
+
+    }
+
+    return finalResult
+
+}
+
+const getExpenseByWeek=(items)=>{
+    const finalResult= []
+    let uniqueDate={}
+    items.forEach((ele)=>{
+
+        var dayDate =getWeekDate(ele.date)
+        uniqueDate[dayDate]=[]
+    })
+
+    items.forEach((ele)=>{
+        let correspindingDate= getWeekDate (ele.date)
+        uniqueDate[correspindingDate].push(ele)
+    })
+
+    for (let x in uniqueDate) {
+        let sum = 0
+        uniqueDate[x].forEach((item) => {
+            sum = sum + Number(item.price)
+        })
+        const expense = {
+            title: "Week: "+x,
+            totalPrice: sum,
+            other: uniqueDate[x]
+        }
+        finalResult.push(expense)
+
+    }
+
+    return finalResult
+
+}
+
+const getTotalPrice=(item)=>{
+    let result=0
+    item.forEach(ele=> {
+        result=result+ item.totalPrice
+    })
+    return result
+
+}
+
+
+
 export{
-    getListOfCategory
+    getListOfCategory,
+    getExpenseByWeek,
+    getExpenseByDay,
+    getTotalPrice
 }
